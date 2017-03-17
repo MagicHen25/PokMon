27c7d33e29b7c3b926f18be597f893562eab9e84
Jack <cl7416@126.com>
2017-03-17 19:43:13 +0800

    commit test

--- func.py
+++ func.py
@@ -1,12 +1,24 @@
 class Role(object):
-    pass
+    def __init__(self, name, blood, weapon, armor):
+        self.name = name
+        self.blood = blood
+        self.weapon = weapon
+        self.armor = armor
+
+    def talk(self):
+        print("你好，我是 %s, 很高兴见到你~" % self.name)
 
 
 class PlayerRole(Role):
+    def __init__(self, name, blood, weapon, armor, gender, occupation, religion, package):
+        super(PlayerRole,self).__init__(name, blood, weapon, armor)
     pass
 
 
 class BadlyRole(Role):
     pass
 
+
+class LittleElf(object):
+    pass
 
---
+++ main.py
@@ -0,0 +1,6 @@
+
+import func
+
+NpcOnion = func.Role('太阳骑士', 200, '黑暗剑', '8')
+
+NpcOnion.talk()
